e22d9bc7bd214b9902718c1821addd659718b066
DeniBrkic <deni.brkic@hotmail.com>
2023-12-24 10:55:59 +0100

    first commit

---
+++ src/zadatakreturn/zadatakreturn.java
@@ -0,0 +1,22 @@
+package zadatakreturn;
+
+public class zadatakreturn {
+
+	public static void main(String[] args) {
+		int x = 10;
+
+		int y = myFunc(x);
+
+		System.out.println(y);
+
+		}
+
+		public static int myFunc(int x) {
+
+		return x*3;
+
+		}
+
+	}
+
+
